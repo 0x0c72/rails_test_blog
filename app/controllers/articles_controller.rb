6bf1b4b8c37e5a7cca9a6800fe5d53454f51c317
Chris Shenkan <cshenkan@shenkan-associates.com>
2014-04-26 00:14:14 -0400

    Added delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,12 +11,29 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
-	
+	def edit
+		@article = Article.find(params[:id])
+	end
 	def show
 		@article = Article.find(params[:id])
 	end
 	def index
 		@articles = Article.all
+	end
+	def update
+		@article = Article.find(params[:id])
+		
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		
+		redirect_to articles_path
 	end
 	private
 		def article_params
